65b556d5ece3567b08784edd6c34527935455cfe
Sam Flavin <flavi022@d.umn.edu>
2019-06-19 10:02:29 -0500

    no stretch

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ let pantryContents  = {
 
 let refrigeratorContents  = {
   eggs: 4,
-  baconStrips: 8,
+  baconStrips: 12,
   chocolateChips: 200,
   cheeseSlices: 6,
   tomatoes: 1,
@@ -14,15 +14,29 @@ let refrigeratorContents  = {
 
 // You shouldn't need to change anything above this line initially
 // You may change it to test different scenarios, but it should work as is
+let eggsNeeded = 8;
+let baconNeeded = 12;
+let panBagsNeeded = 1;
+let cChipsNeeded = 60;
 
 function breakfastChecker(contents) {
-  // Your code goes here
+
+  if (contents.eggs >= eggsNeeded & contents.baconStrips >= baconNeeded 
+    || contents.pancakeMixBags >= panBagsNeeded & contents.chocolateChips >= cChipsNeeded){
+    return true
+  }//egg check done
+  else{
+    return false
+  }//not enough
+}//breakfast checker done
+  
+  
 
 
 
 
 
-}
+
 
 // Your function should return 'yes' or 'no'
 console.log('Does the pantry have enough food for breakfast?', breakfastChecker(pantryContents));
